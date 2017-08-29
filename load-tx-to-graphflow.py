c80200abed1026212e047d1baddae573e4a6a4f5
guoarthur <arthur.guo@uwaterloo.ca>
2017-08-29 17:22:47 -0400

    Fix loading

--- load-tx-to-graphflow.py
+++ load-tx-to-graphflow.py
@@ -4,7 +4,7 @@ import csv
 walletsMap={} #address -> number OR transaction_id->number
 lastNumber = 0
 
-with open(IN_TRANSACTION_CSV_LOCATION, 'rb') as tx_in_file:
+with open(IN_TRANSACTION_CSV_LOCATION, 'r') as tx_in_file:
     in_reader = csv.reader(tx_in_file, delimiter=",")
     for row in in_reader:
         tx_hash = row[0]
@@ -25,9 +25,9 @@ with open(IN_TRANSACTION_CSV_LOCATION, 'rb') as tx_in_file:
             walletsMap[tx_hash] = tx_id
             lastNumber+=1
 
-        print("CREATE ("+str(wallet_id)+":wallet {address: \""+wallet_addr+"\"}) -[:SENT {satoshi: "+str(tx_amt)+"}]->("+str(tx_id)+":tx {hash:\""+tx_hash+"\"})")
+        print("CREATE ("+str(wallet_id)+":Wallet {address: \""+wallet_addr+"\"})-[:SENT {satoshi: \""+str(tx_amt)+"\"}]->("+str(tx_id)+":Tx {hash:\""+tx_hash+"\"});")
 
-with open(OUT_TRANSACTION_CSV_LOCATION, 'rb') as tx_out_file:
+with open(OUT_TRANSACTION_CSV_LOCATION, 'r') as tx_out_file:
     out_reader = csv.reader(tx_out_file, delimiter=",")
     for row in out_reader:
         tx_hash = row[0]
@@ -49,4 +49,4 @@ with open(OUT_TRANSACTION_CSV_LOCATION, 'rb') as tx_out_file:
             lastNumber+=1
 
 
-        print("CREATE ("+str(wallet_id)+":wallet {address: \""+wallet_addr+"\"}) -[:RECEIVED {satoshi: "+str(tx_amt)+"}]->("+str(tx_id)+":tx {hash:\""+tx_hash+"\"})")
+        print("CREATE ("+str(wallet_id)+":Wallet {address: \""+wallet_addr+"\"})-[:RECEIVED {satoshi: \""+str(tx_amt)+"\"}]->("+str(tx_id)+":Tx {hash:\""+tx_hash+"\"});")
